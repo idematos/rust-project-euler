a383e4ab5af701ea2dd31adb9bccd616b6606eb3
Isabela de Matos <30164700+idematos@users.noreply.github.com>
2024-09-27 16:20:45 +0200

    Add Problem 17

---
+++ src/bin/problem017.rs
@@ -0,0 +1,38 @@
+fn number_to_words(n: usize) -> String {
+    let under_twenty = vec![
+        "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven",
+        "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen",
+    ];
+    let tens = vec![
+        "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety",
+    ];
+
+    if n < 20 {
+        return under_twenty[n].to_string();
+    } else if n < 100 {
+        return format!("{}{}", tens[n / 10], under_twenty[n % 10]);
+    } else if n < 1000 {
+        let remainder = n % 100;
+        let hundreds = format!("{}hundred", under_twenty[n / 100]);
+        if remainder > 0 {
+            return format!("{}and{}", hundreds, number_to_words(remainder));
+        } else {
+            return hundreds;
+        }
+    } else if n == 1000 {
+        return "onethousand".to_string();
+    }
+
+    String::new()
+}
+
+fn main() {
+    let mut letter_count = 0;
+    
+    for n in 1..=1000 {
+        let word = number_to_words(n);
+        letter_count += word.len();
+    }
+
+    println!("The total number of letters is {}", letter_count);
+}
