a034d6c1510045ac23e913079848764218049504
Isabela de Matos <30164700+idematos@users.noreply.github.com>
2024-09-27 16:31:33 +0200

    Problem 19

---
+++ src/bin/problem019.rs
@@ -0,0 +1,27 @@
+fn is_leap_year(year: i32) -> bool {
+    (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+}
+
+fn main() {
+    let month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    let mut day_of_week = 1; 
+    let mut sunday_count = 0;
+
+    for year in 1901..=2000 {
+        for (month, &days) in month_days.iter().enumerate() {
+            if day_of_week == 0 {
+                sunday_count += 1;
+            }
+
+            let mut days_in_month = days;
+            if month == 1 && is_leap_year(year) {
+                days_in_month += 1; 
+            }
+
+            day_of_week = (day_of_week + days_in_month) % 7;
+        }
+    }
+
+    println!("The number of sundays on the first of the month is {}", sunday_count);
+}
